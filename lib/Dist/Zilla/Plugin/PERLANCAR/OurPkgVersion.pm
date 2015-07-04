00296627f68425b3016aa8e3a1e36756f350f519
perlancar (@pc-office) <perlancar@gmail.com>
2015-07-04 09:51:45 +0700

    Don't replace #VERSION below __DATA__ or __END__

--- lib/Dist/Zilla/Plugin/PERLANCAR/OurPkgVersion.pm
+++ lib/Dist/Zilla/Plugin/PERLANCAR/OurPkgVersion.pm
@@ -9,62 +9,74 @@ use warnings;
 
 use Moose;
 with (
-	'Dist::Zilla::Role::FileMunger',
-	'Dist::Zilla::Role::FileFinderUser' => {
-		default_finders => [ ':InstallModules', ':ExecFiles' ],
-	},
+    'Dist::Zilla::Role::FileMunger',
+    'Dist::Zilla::Role::FileFinderUser' => {
+        default_finders => [ ':InstallModules', ':ExecFiles' ],
+    },
 );
 
 use namespace::autoclean;
 
 sub munge_files {
-	my $self = shift;
+    my $self = shift;
 
-	$self->munge_file($_) for @{ $self->found_files };
-	return;
+    $self->munge_file($_) for @{ $self->found_files };
+    return;
 }
 
 sub munge_file {
-	my ( $self, $file ) = @_;
+    my ( $self, $file ) = @_;
 
-	if ( $file->name =~ m/\.pod$/ixms ) {
-		$self->log_debug( 'Skipping: "' . $file->name . '" is pod only');
-		return;
-	}
+    if ( $file->name =~ m/\.pod$/ixms ) {
+        $self->log_debug( 'Skipping: "' . $file->name . '" is pod only');
+        return;
+    }
 
-	my $version = $self->zilla->version;
+    my $version = $self->zilla->version;
 
-	my $content = $file->content;
+    my $content = $file->content;
 
-        my $munged_version = 0;
-        $content =~ s/
-                  ^
-                  (\s*)           # capture all whitespace before comment
+    my $end_pos = $content =~ /^(__DATA__|__END__)$/m ? $-[0] : undef;
+    $self->log_debug(["end_pos: %s", $end_pos]);
 
-                  (?:our [ ] \$VERSION [ ] = [ ] 'v?[0-9_.]+'; [ ] )?  # previously produced output
-                  (
-                    \#\s*VERSION  # capture # VERSION
-                    \b            # and ensure it ends on a word boundary
-                    [             # conditionally
-                      [:print:]   # all printable characters after VERSION
-                      \s          # any whitespace including newlines see GH #5
-                    ]*            # as many of the above as there are
-                  )
-                  $               # until the EOL}xm
-		/
-                    "${1}our \$VERSION = '$version'; $2"/emx and $munged_version++;
+    my $munged_version = 0;
+    $content =~ s/
+                     ^
+                     (\s*)           # capture all whitespace before comment
 
-	if ( $munged_version ) {
-		$self->log_debug([ 'adding $VERSION assignment to %s', $file->name ]);
-                $file->content($content);
-	}
-	else {
-		$self->log( 'Skipping: "'
-			. $file->name
-			. '" has no "# VERSION" comment'
-			);
-	}
-	return;
+                     (?:our [ ] \$VERSION [ ] = [ ] 'v?[0-9_.]+'; [ ] )?  # previously produced output
+                     (
+                         \#\s*VERSION  # capture # VERSION
+                         \b            # and ensure it ends on a word boundary
+                         [             # conditionally
+                             [:print:]   # all printable characters after VERSION
+                             \s          # any whitespace including newlines see GH #5
+                         ]*            # as many of the above as there are
+                 )
+                 $               # until the EOL}xm
+                 /
+
+                     !defined($end_pos) || $-[0] < $end_pos ?
+
+                     "${1}our \$VERSION = '$version'; $2"
+
+                     :
+
+                     $&
+
+                     /emx and $munged_version++;
+
+    if ( $munged_version ) {
+        $self->log_debug([ 'adding $VERSION assignment to %s', $file->name ]);
+        $file->content($content);
+    }
+    else {
+        $self->log( 'Skipping: "'
+                        . $file->name
+                        . '" has no "# VERSION" comment'
+                    );
+    }
+    return;
 }
 __PACKAGE__->meta->make_immutable;
 1;
@@ -93,6 +105,8 @@ This module is like L<Dist::Zilla::Plugin::OurPkgVersion> but can replace the
 previously generated C<our $VERSION = '0.123'; > bit. If the author of
 OurPkgVersion thinks this is a good idea, then perhaps this module will be
 merged with OurPkgVersion.
+
+Comment/directive below C<__DATA__> or C<__END__> will not be replaced.
 
 
 =head1 SEE ALSO
